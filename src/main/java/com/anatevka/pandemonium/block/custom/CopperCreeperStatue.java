490fa6346ce773416264bdb284feccbc67ff48b7
anatevka-xD <j.d.deats@gmail.com>
2025-04-10 12:11:27 -0800

    Made CopperStatue Class to inherit from
    Statues now inherit from a generic CopperStatue class
    Cleaned up unused imports

--- src/main/java/com/anatevka/pandemonium/block/custom/CopperCreeperStatue.java
+++ src/main/java/com/anatevka/pandemonium/block/custom/CopperCreeperStatue.java
@@ -3,30 +3,22 @@ package com.anatevka.pandemonium.block.custom;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.WeatheringCopper;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class CopperCreeperStatue extends HorizontalDirectionalBlock implements WeatheringCopper {
+public class CopperCreeperStatue extends CopperStatue {
     public static final MapCodec<CopperCreeperStatue> CODEC = RecordCodecBuilder.mapCodec(
             (properties) -> properties.group(WeatherState.CODEC.fieldOf("weathering_state").forGetter(WeatheringCopper::getAge), propertiesCodec())
                     .apply(properties, CopperCreeperStatue::new)
     );
     private static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 12.0, 12.0);
-    private final WeatherState weatherState;
 
-    public CopperCreeperStatue(WeatherState weatherState, Properties properties) {
-        super(properties);
-        this.weatherState = weatherState;
+    public CopperCreeperStatue(WeatherState weatherState, BlockBehaviour.Properties properties) {
+        super(weatherState, properties);
     }
     @Override
     protected MapCodec<? extends CopperCreeperStatue> codec() {
@@ -35,24 +27,6 @@ public class CopperCreeperStatue extends HorizontalDirectionalBlock implements W
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
-    }
-    @Override
-    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        this.changeOverTime(state,level,pos,random);
-    }
-    @Override
-    protected boolean isRandomlyTicking(BlockState state) {
-        return this.weatherState != WeatherState.OXIDIZED;
-    }
-    @Override
-    public WeatherState getAge() {
-        return this.weatherState;
-    }
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
-    }
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING});
     }
 }
 
--- src/main/java/com/anatevka/pandemonium/block/custom/CopperVillagerStatue.java
+++ src/main/java/com/anatevka/pandemonium/block/custom/CopperVillagerStatue.java
@@ -1,41 +1,24 @@
 package com.anatevka.pandemonium.block.custom;
 
-import com.anatevka.pandemonium.block.entity.CopperPedestalBlockEntity;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
 
-public class CopperVillagerStatue extends HorizontalDirectionalBlock implements WeatheringCopper {
+public class CopperVillagerStatue extends CopperStatue {
     public static final MapCodec<CopperVillagerStatue> CODEC = RecordCodecBuilder.mapCodec(
             (properties) -> properties.group(WeatherState.CODEC.fieldOf("weathering_state").forGetter(WeatheringCopper::getAge), propertiesCodec())
                     .apply(properties, CopperVillagerStatue::new)
     );
-    private static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 11.0, 12.0);
-    private final WeatherState weatherState;
+    private static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 12.0, 12.0);
 
-    public CopperVillagerStatue(WeatherState weatherState, Properties properties) {
-        super(properties);
-        this.weatherState = weatherState;
+    public CopperVillagerStatue(WeatherState weatherState, BlockBehaviour.Properties properties) {
+        super(weatherState, properties);
     }
     @Override
     protected MapCodec<? extends CopperVillagerStatue> codec() {
@@ -44,25 +27,6 @@ public class CopperVillagerStatue extends HorizontalDirectionalBlock implements 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
-    }
-    @Override
-    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        this.changeOverTime(state,level,pos,random);
-    }
-    @Override
-    protected boolean isRandomlyTicking(BlockState state) {
-        return this.weatherState != WeatherState.OXIDIZED;
-    }
-    @Override
-    public WeatherState getAge() {
-        return this.weatherState;
-    }
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
-    }
-
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING});
     }
 }
 
